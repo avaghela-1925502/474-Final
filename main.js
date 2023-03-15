715cc66d6e748647a6dfc5d54cef1580c55062d9
Arjun Vaghela <Arjun@MacBook-Pro-212.local>
2023-03-14 23:14:01 -0700

    done

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const dataInput = d3.select("#chart")
   .on("change", updateChart);
 
 dataInput.selectAll("option")
-  .data(["Seattle", "New York City", "Houston"])
+  .data(["Choose City", "Seattle", "New York City", "Houston"])
   .enter()
   .append("option")
   .attr("value", d => d)
@@ -39,7 +39,9 @@ dataInput.selectAll("option")
 const dropdownWidth = dataInput.node().getBoundingClientRect().width;
 dataInput.style("position", "absolute")
   .style("top", "50px")
-  .style("right", `${10 + dropdownWidth}px`);
+  .style("right", `${500 + dropdownWidth}px`);
+
+updateChart();
 
 function updateChart() {
     const selectedData = d3.select("#data-input").property("value");
@@ -47,6 +49,10 @@ function updateChart() {
     let color;
 
     // Add a conditional statement for "KHOU.csv"
+    if (selectedData === "Choose City") {
+        dataFile = "KSEA.csv";
+        color = colors[0];
+    } 
     if (selectedData === "Seattle") {
         dataFile = "KSEA.csv";
         color = colors[0];
@@ -57,98 +63,121 @@ function updateChart() {
         dataFile = "KHOU.csv";
         color = colors[2];
     }
-  
-    // Load the data
-    d3.csv(dataFile).then(data => {
-      // Format the data
-      data = data.map(d => {
-        return {
-          'record_max_temp': d['record_max_temp'],
-          'record_max_temp_year': d['record_max_temp_year']
-        };
-      });
-  
-      // Convert record_max_temp_year to dates
-      const parseDate = d3.timeParse("%Y");
-      data.forEach(d => {
-        d.record_max_temp_year = parseDate(d.record_max_temp_year);
-      });
-  
-      // Set up the scales
-      const xScale = d3.scaleBand()
-        .domain(data.map(d => d.record_max_temp_year).sort((a, b) => a - b))
-        .range([0, width])
-        .padding(0.2);
-  
-      const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.record_max_temp)])
-        .range([height, 0]);
-  
 
-    // Draw the axes
-const xAxis = d3.axisBottom(xScale)
-  .tickFormat(d3.timeFormat("%Y"))
-  .tickSizeOuter(0)
-  .tickPadding(10);
-svg.select(".x-axis")
-  .transition()
-  .duration(500)
-  .call(xAxis)
-  .selectAll("text")
-  .attr("transform", "rotate(-90)")
-  .attr("dx", "-2.0em")
-  .attr("dy", "-0.60em")
-  .style("text-anchor", "end"); // set the text anchor to the end of the label
-
-const yAxis = d3.axisLeft(yScale)
-  .tickSizeOuter(0)
-  .tickPadding(10);
-svg.select(".y-axis")
-  .transition()
-  .duration(500)
-  .call(yAxis);
-
-// Add x-axis label
-svg.append("text")
-  .attr("class", "x-axis-label")
-  .attr("x", width / 2)
-  .attr("y", height + 70)
-  .style("text-anchor", "middle")
-  .text("Year")
-
-// Add y-axis label
-svg.append("text")
-  .attr("class", "y-axis-label")
-  .attr("transform", "rotate(-90)")
-  .attr("x", -height / 2)
-  .attr("y", -50)
-  .style("text-anchor", "middle")
-  .text("Temperature (Â°F)");
-
+// Load the data
+d3.csv(dataFile).then(data => {
+    // Format the data
+    data = data.map(d => {
+    return {
+      'record_max_temp': d['record_max_temp'],
+      'record_max_temp_year': new Date(d['record_max_temp_year'])
+    };
+  });
   
-// Draw the bars
-const bars = svg.selectAll("rect")
-    .data(data);
-
-bars.enter()
-    .append("rect")
-    .merge(bars)
-    .transition()
-    .duration(500)
-    .attr("x", d => xScale(d.record_max_temp_year))
-    .attr("y", d => yScale(d.record_max_temp))
-    .attr("width", xScale.bandwidth())
-    .attr("height", d => {
-        if (isNaN(d.record_max_temp)) {
-        return 0;
-        } else {
-        return height - yScale(d.record_max_temp);
-        }
-    })
-    .attr("fill", color);
+  
+    // Compute the average temperature per year
+    const avgTempByYear = d3.rollup(data, 
+      group => d3.mean(group, d => d.record_max_temp), 
+      d => d.record_max_temp_year.getFullYear()
+    );
+  
+    // Convert the data to an array of objects
+    data = Array.from(avgTempByYear, ([year, value]) => ({ year, value }));
+  
+    // Convert the year string to a Date object
+    const parseDate = d3.timeParse("%Y");
+    data.forEach(d => {
+      d.year = parseDate(d.year);
+    });
+  
+    // Set up the scales
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(data, d => d.year))
+      .range([0, width]);
+  
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.value)])
+      .range([height, 0]);
+  
+    const tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "10px");
     
 
-bars.exit().remove();
 
-});
+    // Draw the axes
+    const xAxis = d3.axisBottom(xScale)
+      .tickSizeOuter(0)
+      .tickPadding(10);
+    svg.select(".x-axis")
+      .transition()
+      .duration(500)
+      .call(xAxis)
+      .selectAll("text")
+      .attr("transform", "rotate(-90)")
+      .attr("dx", "-2.0em")
+      .attr("dy", "-0.60em")
+      .style("text-anchor", "end"); // set the text anchor to the end of the label
+  
+    const yAxis = d3.axisLeft(yScale)
+      .tickSizeOuter(0)
+      .tickPadding(10);
+    svg.select(".y-axis")
+      .transition()
+      .duration(500)
+      .call(yAxis);
+  
+    // Add x-axis label
+    svg.append("text")
+      .attr("class", "x-axis-label")
+      .attr("x", width / 2)
+      .attr("y", height + 70)
+      .style("text-anchor", "middle")
+      .text("Year")
+  
+    // Add y-axis label
+    svg.append("text")
+      .attr("class", "y-axis-label")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height / 2)
+      .attr("y", -50)
+      .style("text-anchor", "middle")
+      .text("Temperature (°F)");
+  
+    // Draw the bars
+    const circles = svg.selectAll("circle")
+    .data(data);
+
+    circles.enter()
+        .append("circle")
+        .merge(circles)
+    
+    circles.transition()
+        .duration(500)
+        .attr("cx", d => xScale(d.year))
+        .attr("cy", d => yScale(d.value))
+        .attr("r", 5)
+        .attr("fill", color);
+    
+    circles.on('mouseover', function (event, d) {
+        d3.select(this).transition()
+             .duration('50')
+             .attr('opacity', '0.5');
+        tooltip.transition()
+            .duration('50')
+            .text(d.value.toFixed(2) + "°F |   " + d.year)
+            .style("visibility", "visible");
+            console.log(d)
+    });
+
+    circles.exit().remove();
+  });
+  
 }
